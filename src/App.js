9bc62d83158864531ca6bedbb8b1e07c3c15be70
gitihm <ismael897@outlook.com>
2019-01-17 23:19:42 +0700

    add auth0

--- src/App.js
+++ src/App.js
@@ -3,26 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div><center>
+          <a href="https://airx.auth0.com/login?state=g6Fo2SBOcG5FNlExY0NMYjZWdDUxNnJabmZ3YzlzbFhaLTg2SqN0aWTZIDFJWmRWLXpFN2ZIWDlWRk8xSUZQN0t6OXpaMXc3MmJ4o2NpZNkgNlZaNk1sQ1Zta1hzMVRPdVlLdzRSSWhRcTk5RXU5Znc&client=6VZ6MlCVmkXs1TOuYKw4RIhQq99Eu9fw&protocol=oauth2&audience=http%3A%2F%2Flocalhost%3A3000&scope=SCOPE&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin"><button>Click</button></a></center>
       </div>
-    );
-  }
+    )}
 }
 
 export default App;
